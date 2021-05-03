badbfcbdb72ab58f74259d0867244f7ce60134d4
ramy <ramysmelo@gmail.com>
2021-05-03 16:42:45 -0400

    Setup Testing Library and setup a utils module for tests to setup plugins.

--- test/Logo.spec.ts
+++ test/Logo.spec.ts
@@ -1,9 +1,10 @@
-import { mount } from '@vue/test-utils'
+import '@testing-library/jest-dom'
 import Logo from '@/components/Logo.vue'
+import { render } from '@testing-library/vue'
 
 describe('Logo', () => {
   test('is a Vue instance', () => {
-    const wrapper = mount(Logo)
-    expect(wrapper.vm).toBeTruthy()
+    const wrapper = render(Logo)
+    expect(wrapper).toBeTruthy()
   })
 })
