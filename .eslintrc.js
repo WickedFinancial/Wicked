2e4d840f57cc378bfec548e18bb2e2545bc8a4af
ramy <ramysmelo@gmail.com>
2021-05-05 12:45:48 -0400

    Formatting changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,13 +5,14 @@ module.exports = {
     node: true,
   },
   extends: [
-    '@nuxtjs/eslint-config-typescript',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended',
+    "@nuxtjs/eslint-config-typescript",
+    "plugin:prettier/recommended",
+    "plugin:nuxt/recommended",
+    "eslint:recommended",
   ],
   plugins: [],
   // add your custom rules here
   rules: {
-    '@typescript-eslint/no-empty-interface': 1,
+    "@typescript-eslint/no-empty-interface": 1,
   },
-}
+};
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,22 +1,22 @@
-import colors from 'vuetify/es5/util/colors'
+import colors from "vuetify/es5/util/colors";
 
 export default {
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: "static",
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - avow',
-    title: 'avow',
+    titleTemplate: "%s - avow",
+    title: "avow",
     htmlAttrs: {
-      lang: 'en',
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -31,17 +31,17 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
+    "@nuxt/typescript-build",
     // https://go.nuxtjs.dev/vuetify
-    '@nuxtjs/vuetify',
-    'nuxt-purgecss',
-    'nuxt-typed-vuex',
+    "@nuxtjs/vuetify",
+    "nuxt-purgecss",
+    "nuxt-typed-vuex",
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
@@ -49,7 +49,7 @@ export default {
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
+    customVariables: ["~/assets/variables.scss"],
     treeShake: true,
     theme: {
       dark: true,
@@ -68,18 +68,18 @@ export default {
   },
 
   purgeCSS: {
-    mode: 'webpack',
+    mode: "webpack",
     enabled: ({ isDev }) => !isDev, // or `false` when in dev/debug mode
     paths: [
-      'components/**/*.vue',
-      'layouts/**/*.vue',
-      'pages/**/*.vue',
-      'plugins/**/*.js',
-      'node_modules/vuetify/src/**/*.ts',
-      'node_modules/vuetify/dist/vuetify.css',
+      "components/**/*.vue",
+      "layouts/**/*.vue",
+      "pages/**/*.vue",
+      "plugins/**/*.js",
+      "node_modules/vuetify/src/**/*.ts",
+      "node_modules/vuetify/dist/vuetify.css",
     ],
-    styleExtensions: ['.css'],
-    whitelist: ['body', 'html', 'nuxt-progress'],
+    styleExtensions: [".css"],
+    whitelist: ["body", "html", "nuxt-progress"],
     whitelistPatterns: [
       /-(leave|enter|appear)(|-(to|from|active))$/,
       /^(?!(|.*?:)cursor-move).+-move$/,
@@ -91,16 +91,16 @@ export default {
         extractor: (content) => {
           const contentWithoutStyleBlocks = content.replace(
             /<style[^]+?<\/style>/gi,
-            ''
-          )
+            ""
+          );
 
           return (
             contentWithoutStyleBlocks.match(
               /[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g
             ) || []
-          )
+          );
         },
-        extensions: ['html', 'vue', 'js', 'ts'],
+        extensions: ["html", "vue", "js", "ts"],
       },
     ],
   },
@@ -108,7 +108,7 @@ export default {
   build: {
     extractCSS: true,
     babel: {
-      plugins: [['@babel/plugin-proposal-private-methods', { loose: true }]],
+      plugins: [["@babel/plugin-proposal-private-methods", { loose: true }]],
     },
   },
-}
+};
