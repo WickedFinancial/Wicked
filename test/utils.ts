8e7405b96fcdd0f1a5ce06f483e41a6e1910064d
ramy <ramysmelo@gmail.com>
2021-06-01 14:19:30 -0400

    Linting

--- test/utils.ts
+++ test/utils.ts
@@ -1,22 +1,22 @@
-import Vuetify from 'Vuetify'
+import Vuetify from "Vuetify"
 import {
   ConfigurationCallback,
   render,
   RenderOptions,
   RenderResult,
-} from '@testing-library/vue'
-import Vue from 'vue'
+} from "@testing-library/vue"
+import Vue from "vue"
 
 Vue.use(Vuetify)
-Vue.component('NuxtLink', { template: '<a></a>' }) // stubbing out nuxt-link
+Vue.component("NuxtLink", { template: "<a></a>" }) // stubbing out nuxt-link
 
 function renderWithVuetify<VueComponent>(
   component: VueComponent,
   options?: RenderOptions<never>,
   callback?: ConfigurationCallback<never>
 ): RenderResult {
-  const root = document.createElement('div')
-  root.setAttribute('data-app', 'true')
+  const root = document.createElement("div")
+  root.setAttribute("data-app", "true")
   const mergedOptions = {
     container: document.body.appendChild(root),
     // for Vuetify components that use the $vuetify instance property
