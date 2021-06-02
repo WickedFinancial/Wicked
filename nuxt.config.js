272205482bd37b5b870d34c2fe2634a824b405b3
ramy <ramysmelo@gmail.com>
2021-06-02 18:34:14 -0400

    disabling axios until we need it.

--- nuxt.config.js
+++ nuxt.config.js
@@ -41,11 +41,11 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    "@nuxtjs/axios",
+    //   "@nuxtjs/axios",
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
+  // axios: {},
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
