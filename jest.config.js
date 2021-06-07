c6184d40fadee5d5561c4d9a0f9dfd1bcae73411
ramy <ramysmelo@gmail.com>
2021-06-07 14:10:10 -0400

    Updated packages, made change to jest in order to conform to a breaking change. Our default test enviroment is jestdom vs node.

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,7 @@ module.exports = {
     "^.+\\.js$": "babel-jest",
     ".*\\.(vue)$": "vue-jest",
   },
+  testEnvironment: "jsdom",
   collectCoverage: true,
   collectCoverageFrom: [
     "<rootDir>/components/**/*.vue",
