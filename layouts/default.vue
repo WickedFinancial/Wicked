3c2c7951d176a754b86d07339a62df5f2873a4fb
ramy <ramysmelo@gmail.com>
2021-06-08 11:36:48 -0400

    Playing around with the default layout. Added the vuetify default assets back in.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,27 +1,68 @@
 <template>
   <v-app dark>
-    <v-app-bar fixed app>
-      <div id="site-title" class="text-md-h3 d-md-inline">
-        {{ title }}
-      </div>
+    <v-navigation-drawer
+      v-model="drawer"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+
+      <v-btn icon @click.stop="clipped = !clipped">
+        <v-icon>mdi-application</v-icon>
+      </v-btn>
+      <v-toolbar-title v-text="title" />
       <avow-logo id="Logo" />
       <v-spacer />
-      <v-btn>Connect</v-btn>
     </v-app-bar>
     <v-main>
-      <nuxt />
+      <v-container>
+        <nuxt />
+      </v-container>
     </v-main>
-    <v-footer app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
 
-<script lang="ts">
-import { Vue, Component } from "vue-property-decorator"
-
-@Component
-export default class DefaultLayout extends Vue {
-  title = "Avow"
+<script>
+export default {
+  data() {
+    return {
+      clipped: true,
+      drawer: true,
+      items: [
+        {
+          icon: "mdi-apps",
+          title: "Dashboard",
+          to: "/",
+        },
+        // {
+        //   icon: "mdi-chart-bubble",
+        //   title: "Mint",
+        //   to: "/mint",
+        // },
+      ],
+      right: true,
+      rightDrawer: false,
+      title: "Avow",
+    }
+  },
 }
 </script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -49,7 +49,7 @@ export default {
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    defaultAssets: false,
+    // defaultAssets: false,
     customVariables: ["~/assets/variables.scss"],
     treeShake: true,
     theme: {
