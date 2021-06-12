e250b206f8c3f4ce891187b3a416d57c1003e602
ramy <ramysmelo@gmail.com>
2021-06-12 17:39:34 -0400

    Added a barebones hardhat config.
    ToDo: Make it default that testing hardhat changes jest env to node. For now I expect more tests for the frontend than hardhat.

---
+++ test/sample-test.spec.ts
@@ -0,0 +1,18 @@
+/**
+ * @jest-environment node
+ */
+
+import { ethers } from "hardhat"
+
+describe("Greeter", function () {
+  it("Should return the new greeting once it's changed", async function () {
+    const Greeter = await ethers.getContractFactory("Greeter")
+    const greeter = await Greeter.deploy("Hello, world!")
+
+    await greeter.deployed()
+    expect(await greeter.greet()).toBe("Hello, world!")
+
+    await greeter.setGreeting("Hola, mundo!")
+    expect(await greeter.greet()).toBe("Hola, mundo!")
+  })
+})
