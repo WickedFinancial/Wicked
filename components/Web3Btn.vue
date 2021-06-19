5c7f80acb861908d60d2710d54e157e781b0bb3e
ramy <ramysmelo@gmail.com>
2021-06-19 11:23:02 -0400

    WIP: Web3 Button Component.

---
+++ components/Web3Btn.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-btn role="button" @click.p="toggleConnectionStatus">
+    <v-icon>mdiEthereum</v-icon>
+    {{ connectionStatus }}
+  </v-btn>
+</template>
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator"
+
+@Component
+export default class Web3Btn extends Vue {
+  isConnected = false
+
+  get connectionStatus() {
+    return this.isConnected ? "Disconnect" : "Connect"
+  }
+
+  set connectionStatus(isConnected: boolean) {
+    this.isConnected = isConnected
+  }
+
+  toggleConnectionStatus() {
+    this.connectionStatus = !this.isConnected
+  }
+}
+</script>
+<style scoped></style>
---
+++ test/Web3Btn.spec.ts
@@ -0,0 +1,25 @@
+import "@testing-library/jest-dom"
+import { screen, fireEvent } from "@testing-library/vue"
+import Web3BtnComponent from "~/components/Web3Btn.vue"
+import { renderWithVuetify as render } from "~/test/utils"
+
+describe("Web3Btn", () => {
+  let btn: HTMLElement
+  beforeEach(() => {
+    btn = render(Web3BtnComponent).getByRole("button")
+  })
+  it("should be on the screen and accessible by role button", () => {
+    expect(btn).toBeInTheDocument()
+  })
+  it("should match a snapshot", () => {
+    expect(btn).toMatchSnapshot()
+  })
+  it("should be disconnected by default", () => {
+    expect(screen.getByText("Connect"))
+  })
+  it("should toggle status on click", async () => {
+    expect(btn).toHaveTextContent("Connect")
+    await fireEvent.click(btn)
+    expect(btn).toHaveTextContent("Disconnect")
+  })
+})
