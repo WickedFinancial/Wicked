da56b12589eaf20a3dd011aaf1d3ee7f3e9c1f46
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-06-20 15:56:56 +0200

    Add links for price feed and collateral

--- pages/contract/_contract.vue
+++ pages/contract/_contract.vue
@@ -1,8 +1,10 @@
 <template>
   <v-container>
-    <v-card :loading="loading" class="mx-auto my-12" max-width="500">
+    <v-card class="mx-auto my-12" max-width="500">
       <v-card-title
-        ><a :href="etherscanLink">{{ this.contractName }}</a></v-card-title
+        ><a :href="etherscanLinkContract">{{
+          this.contractName
+        }}</a></v-card-title
       >
       <v-card-subtitle>{{ this.contractType }}</v-card-subtitle>
       <v-card-text>
@@ -13,9 +15,11 @@
 
         <v-list-item>
           <v-list-item-title> Collateral </v-list-item-title>
-          <v-list-item-subtitle>{{
-            this.contractDetails.collateralToken
-          }}</v-list-item-subtitle>
+          <v-list-item-subtitle
+            ><a :href="etherscanLinkCollateral">{{
+              this.contractDetails.collateralToken
+            }}</a></v-list-item-subtitle
+          >
         </v-list-item>
 
         <v-list-item>
@@ -27,9 +31,19 @@
 
         <v-list-item>
           <v-list-item-title> Price Feed </v-list-item-title>
-          <v-list-item-subtitle>{{
-            this.contractDetails.priceIdentifier
-          }}</v-list-item-subtitle>
+          <v-list-item-subtitle
+            ><a :href="priceFeedLink">{{
+              this.contractDetails.priceIdentifier
+            }}</a></v-list-item-subtitle
+          >
+        </v-list-item>
+
+        <v-list-item
+          v-for="(parameterInfo, index) in libraryConfiguration"
+          :key="index"
+        >
+          <v-list-item-title>{{ parameterInfo.name }}</v-list-item-title>
+          <v-list-item-subtitle>{{ parameterInfo.value }}</v-list-item-subtitle>
         </v-list-item>
       </v-card-text>
     </v-card>
@@ -39,11 +53,21 @@
 <script lang="ts">
 import { Vue, Component, namespace } from "nuxt-property-decorator"
 import { LSPConfiguration } from "@/types"
+const addresses: Record<string, string> = require("@/addresses.json")
 const contracts = namespace("contracts")
 
 const readableLibraryNames: Record<string, string> = {
   LinearLongShortPairFinancialProductLibrary: "Linear Payout Contract",
 }
+
+const libraryParameters: Record<string, Array<string>> = {
+  LinearLongShortPairFinancialProductLibrary: ["Upper Bound", "Lower Bound"],
+}
+
+const priceFeedLinks: Record<string, string> = {
+  USDETH: "https://github.com/UMAprotocol/UMIPs/blob/master/UMIPs/umip-6.md",
+}
+
 @Component
 export default class contract extends Vue {
   @contracts.State
@@ -61,9 +85,38 @@ export default class contract extends Vue {
     }
   }
 
-  get etherscanLink(): string | undefined {
+  get libraryConfiguration(): Array<object> | undefined {
+    if (this.contractDetails) {
+      return this.contractDetails.financialProductLibraryParameters.map(
+        (value, index) => {
+          return {
+            name: libraryParameters[
+              this.contractDetails.financialProductLibrary
+            ][index],
+            value,
+          }
+        }
+      )
+    }
+  }
+
+  get etherscanLinkContract(): string | undefined {
     if (this.contractDetails) {
       return `https://kovan.etherscan.io/address/${this.contractDetails.address}`
+    }
+  }
+
+  get etherscanLinkCollateral(): string | undefined {
+    if (this.contractDetails) {
+      return `https://kovan.etherscan.io/address/${
+        addresses[this.contractDetails.collateralToken]
+      }`
+    }
+  }
+
+  get priceFeedLink(): string | undefined {
+    if (this.contractDetails) {
+      return priceFeedLinks[this.contractDetails.priceIdentifier]
     }
   }
 
