1fda8d86f4bbdbe71643ea600bd47435ff10e38c
Guillermo Sainz <guillermo@sketchfab.com>
2021-06-26 23:05:46 -0400

    Improves button styling

--- pages/index.vue
+++ pages/index.vue
@@ -39,7 +39,7 @@
           outlined
           to="/app"
         >
-          Use App <v-icon>mdi-chevron-double-right</v-icon>
+          Use App <v-icon>mdi-chevron-right</v-icon>
         </v-btn>
       </v-col>
     </v-row>
@@ -53,7 +53,7 @@ export default class Index extends Vue {}
 </script>
 <style type="text/css">
 ul{
-  margin-top:10px;
+  margin-top:20px;
 }
 li{
   list-style: none !important;
@@ -64,6 +64,15 @@ li i{
 }
 .use-app{
   margin: 30px 0 !important;
+  background: white;
+  border-radius: 0;
+  color: black !important;
+  text-transform: capitalize;
+  font-size: 12px;
+  padding: 25px !important;
+  font-weight: bolder;
+  letter-spacing: 0;
+  cursor: pointer;
 }
 #app{
   background: #151e2a !important;
@@ -71,6 +80,13 @@ li i{
 }
 h1{
   font-weight: bolder;
+  font-size: 46px;
+  padding: 30px 0;
+  line-height: 55px;
+}
+h3{
+  font-weight: 300;
+  font-size: 18px;
 }
 h1, h3{
   background: -webkit-linear-gradient(left, #fff, #cbfdfd);
