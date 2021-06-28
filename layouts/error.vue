d92ab69d6e0d9eceb5ac33f68ef878d42383412f
ramy <ramysmelo@gmail.com>
2021-06-28 12:27:42 -0400

    Made the error layout component typed and use the default layout until we design something more custom.

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,42 +1,30 @@
 <template>
-  <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/"> Home page </NuxtLink>
-  </v-app>
+  <v-container>
+    <h2>Status Code: {{ error.statusCode }}</h2>
+    <h3>Message: {{ error.message }}</h3>
+  </v-container>
 </template>
 
-<script>
-export default {
-  layout: "empty",
-  props: {
-    error: {
-      type: Object,
-      default: null,
-    },
-  },
-  data() {
-    return {
-      pageNotFound: "404 Not Found",
-      otherError: "An error occurred",
-    }
-  },
+<script lang="ts">
+import { Component, Prop, Vue } from "nuxt-property-decorator"
+
+interface AppError {
+  code: number
+  message: string
+  stack: string
+  statusCode: number
+}
+
+@Component
+export default class ErrorLayout extends Vue {
+  @Prop({ default: null }) error: AppError | undefined
+  pageNotFound = "404 Not Found"
+  otherError = "An error occurred"
+
   head() {
     const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
-    return {
-      title,
-    }
-  },
+      this.error?.statusCode === 404 ? this.pageNotFound : this.otherError
+    return { title }
+  }
 }
 </script>
-
-<style scoped>
-h1 {
-  font-size: 20px;
-}
-</style>
