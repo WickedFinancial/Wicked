834f4981dd2a3789b46b6b8610c94d38084ab2ee
Christian Koopmann <christian@Christians-MacBook-Pro.local>
2021-06-30 19:38:15 +0200

    Add section on used technology

--- pages/index.vue
+++ pages/index.vue
@@ -4,23 +4,28 @@
       <v-col cols="12" sm="8" md="6">
         <h1>DeFi Currency Hedging<br />made simple</h1>
         <h3>
-          Manage exchange rate risk using Wicked derivatives.
+          Manage exchange rate risk using on-chain derivatives.
           <br />
         </h3>
         <br />
         <h2>Benefits</h2>
         <ul>
           <li>
-            <v-icon medium color="gray"> mdi-scale-balance</v-icon>
-            Tokenized long / short positions on Exchange Rates (e.g. EUR/USD)
+            <v-icon medium color="gray"> mdi-ethereum</v-icon>
+            Trade long/short positions on Exchange Rates (e.g. EUR/USD) as
+            ERC-20 tokens
           </li>
           <li>
-            <v-icon medium color="gray"> mdi-cash</v-icon>
+            <v-icon medium color="gray"> mdi-percent-outline</v-icon>
             Capital efficiency through leveraged payout structure
           </li>
           <li>
-            <v-icon medium color="gray"> mdi-piggy-bank-outline</v-icon>
+            <v-icon medium color="gray"> mdi-safe</v-icon>
             No Liquidation risk for Liquidity Providers
+          </li>
+          <li>
+            <v-icon medium color="gray"> mdi-exit-run</v-icon>
+            Retrieve collateral pre-expiry by burning long/short token-pairs
           </li>
         </ul>
 
@@ -41,38 +46,78 @@
         </ol>
 
         <br />
-        <h2>Example: </h2>
+        <h2>Example:</h2>
         <ol>
           <li>
-            Sponsor provides 100 LUSD collateral to mint Short / Long Tokens for USD/EUR-contract with bounds 1.10 - 1.30 USD.
+            Sponsor provides 100 LUSD collateral to mint Short / Long Tokens for
+            USD/EUR-contract with bounds 1.10 - 1.30 USD.
           </li>
           <li>User A buys the Long Tokens from the Sponsor for 51 LUSD</li>
           <li>User B buys the Short Tokens from the Sponsor for 51 LUSD</li>
           <li>Contract Expires</li>
           <li>Value of 1.25 is returned by price oracle</li>
-          <li>
-            Payout User A: 100 LUSD * (1.25-1.10)/(1.30-1.1) = 75 LUSD 
-          </li>
-          <li>
-            Payout User B: 100 LUSD * (1.30-1.25)/(1.30-1.1) = 25 LUSD 
-          </li>
+          <li>Payout User A: 100 LUSD * (1.25-1.10)/(1.30-1.1) = 75 LUSD</li>
+          <li>Payout User B: 100 LUSD * (1.30-1.25)/(1.30-1.1) = 25 LUSD</li>
         </ol>
 
         <br />
-        <h2>Wait but why? </h2>
+        <h2>Wait but why?</h2>
         <ol>
+          <li>Sponsors can earn returns through trading fees</li>
           <li>
-            Sponsors can earn returns through trading fees
+            Users get leveraged exposure to the currency of their choice to:
           </li>
-          <li>Users get leveraged exposure to the currency of their choice to:</li>
-          <ol>
-            <li>Hedge an existing currency risk exposure (e.g. USD income but EUR expenditures)</li>
+          <ul>
+            <li>
+              Hedge an existing currency risk exposure (e.g. USD income but EUR
+              expenditures)
+            </li>
             <li>Maximise profitability of their currency speculation</li>
-          </ol>
+          </ul>
         </ol>
 
+        <br />
+        <h2>The technology</h2>
+        <ol>
+          <li>
+            Derivative contracts based on
+            <a href="https://umaproject.org/">UMA</a> Primitives:
+          </li>
+          <ul>
+            <li>
+              General Contract-Lifecycle defined by the Long-Short-Pair
+              Contract:
+              <a
+                href="https://medium.com/uma-project/introducing-umas-long-short-pair-lsp-financial-primitive-84596803864f"
+                >Blogpost</a
+              >,
+              <a
+                href="https://docs.umaproject.org/synthetic-tokens/long-short-pair"
+                >Documentation</a
+              >,
+              <a href="https://www.youtube.com/watch?v=sPb1kOmZgbs"
+                >Video-Explainer</a
+              >
+            </li>
+            <li>
+              Payout structure based on Linear Payout Library:
+              <a
+                href="https://github.com/UMAprotocol/protocol/blob/master/packages/core/contracts/financial-templates/common/financial-product-libraries/long-short-pair-libraries/LinearLongShortPairFinancialProductLibrary.sol"
+                >github</a
+              >
+            </li>
+            <li>
+              Price feeds provided through Optimistic-Oracle:
+              <a href="https://docs.umaproject.org/getting-started/oracle"
+                >Documentation</a
+              >,
+            </li>
+          </ul>
 
-
+          <li>
+            <a href="https://www.liquity.org/">Liquity</a>(LUSD) used as collateral stable-coin for contracts
+          </li>
+        </ol>
       </v-col>
     </v-row>
   </v-container>
